4618b0c2364972aba8f7c12ac9618c3db23fde8a
Korucu <korucu@rose-hulman.edu>
2023-09-12 11:57:25 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './Create';
+import BlogDetail from './BlogsDetails';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetail/>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
